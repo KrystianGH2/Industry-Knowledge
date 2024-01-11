f62a55aae1e4d106dcf97b0af951d035cdefd60c
KrystianGH2 <kgcdronio@gmail.com>
2024-01-11 22:17:39 +0100

    render testing after fetching data from database

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-import UserList from "./(components)/UsersPage"
+import UserList from "./(components)/UsersPage";
+import UserReports from "./(components)/userReports";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Home Page</h1>
-      <UserList/>
+      <UserReports/>
+      <UserList />
     </main>
-  )
+  );
 }
