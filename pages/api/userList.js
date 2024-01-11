1473021497e6bccd6adda36a9fc86ca9d943521d
KrystianGH2 <kgcdronio@gmail.com>
2024-01-11 22:21:27 +0100

    API route to handle the user fetching

--- pages/api/userList.js
+++ pages/api/userList.js
@@ -1,13 +1,9 @@
 import User from "@/app/(models)/User";
-import dbConnect from "../../app/utils/db";
+import dbConnectUsers from "../../app/utils/db";
 
 export default async function handler(req, res) {
   try {
-    console.log("Connecting to the database...");
-    await dbConnect();
-    if (dbConnect.status === "OK") {
-      console.log("Connected to database");
-    }
+    await dbConnectUsers();
 
     const users = await User.find({});
     res.status(200).json({ users });
