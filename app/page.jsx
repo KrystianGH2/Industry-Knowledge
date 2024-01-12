18083ad37e41429b5b80dcb0bd89583aaa9070db
KrystianGH2 <kgcdronio@gmail.com>
2024-01-12 12:31:51 +0100

    edited the userReport name due to site not hot updating

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
+import UserReportList from "./(components)/UserReport";
 import UserList from "./(components)/UsersPage";
-import UserReports from "./(components)/userReports";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
-      <UserReports/>
+      <h1>Home</h1>
+      <UserReportList />
       <UserList />
     </main>
   );
