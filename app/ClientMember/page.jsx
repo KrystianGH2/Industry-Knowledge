ccfd92990904df58715c1b6f17146ce0b3a0393e
KrystianGH2 <kgcdronio@gmail.com>
2024-01-17 15:50:09 +0100

    moved React Context into client side

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,20 +1,7 @@
-"use client";
-import { redirect } from "next/navigation";
-import { useSession } from "next-auth/react";
 import ReportForm from "../(components)/ReportForm";
 const ClientMember = () => {
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect("/api/auth/signin?callbackUrl=/Member");
-    },
-  });
   return (
     <div data-theme="dark">
-      <h1>Member Client Session</h1>
-      <p>{session?.user?.name}</p>
-      <p>{session?.user?.email}</p>
-      <p>{session?.user?.role}</p>
       <ReportForm />
     </div>
   );
