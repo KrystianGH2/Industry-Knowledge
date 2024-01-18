9008fa697dda909e81af5451f7d9b212ad4343d9
KrystianGH2 <kgcdronio@gmail.com>
2024-01-18 14:59:12 +0100

    Styling Updates

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import options from "../api/auth/[...nextauth]/options";
 import Image from "next/image";
 
+import NavLinks from "./NavLinks";
 const Nav = async () => {
   const session = await getServerSession(options);
   return (
@@ -13,44 +14,30 @@ const Nav = async () => {
           data-theme="dark"
           className="bg-[#1A1A1A] flex justify-center items-center"
         >
-          <header className="bg-[#1A1A1A] text-[#8E8E9A] flex  items-center w-full">
-            <nav className="flex w-full justify-between px-10 py-4">
-              <div className="text-xl font-bold">BlockGuardian</div>
-              <div className="flex gap-10">
-                <div className="flex gap-10">
-                  <Link className=" hover:text-white transition" href="/">
-                    Home
-                  </Link>
-                  <Link
-                    className=" hover:text-white transition"
-                    href="/ClientMember"
-                  >
-                    Client Member
-                  </Link>
-                  <Link className=" hover:text-white transition" href="/Member">
-                    Member
-                  </Link>
-                  <Link className=" hover:text-white transition" href="/Public">
-                    Public
-                  </Link>
-                </div>
-                <div className="flex flex-row gap-5">
+          <header className="bg-[#1A1A1A] text-[#8E8E9A] flex  items-center w-full justify-center">
+            <nav className="flex w-full justify-between items-center px-10 py-4 h-20 max-w-7xl">
+              <div className="text-2xl font-bold text-[#c5c5c5] hover:text-[#f1f1f1] transition shadow-">
+                <Link href="/">BlockGuardian</Link>
+              </div>
+              <div className="flex gap-10 justify-center items-center">
+              <NavLinks/>
+                <div className="flex flex-row gap-5 items-center justify-center">
                   {session ? (
                     <>
-                      <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
-                      <Image
-                        className="w-5 h-5"
-                        src="https://www.svgrepo.com/show/31480/notification-bell.svg"
-                        alt="avatar"
-                        width={24} // Replace with the desired width
-                        height={24} // Replace with the desired height
-                      />
+                      <Link href="/api/auth/signout?callbackUrl=/">
+                        <Image
+                          className="w-5 h-5 text-center"
+                          src="https://www.svgrepo.com/show/21304/logout.svg"
+                          alt="avatar"
+                          width={24} // Replace with the desired width
+                          height={24} // Replace with the desired height
+                        />
+                      </Link>
                     </>
                   ) : (
                     <>
-                     
                       <Link
-                        className=" hover:text-white transition"
+                        className=" hover:text-white transition px-3 hover:bg- border rounded p-1 text-center"
                         href="/api/auth/signin"
                       >
                         Login
--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,36 @@
 import UserReportList from "./(components)/UserReport";
 import UserList from "./(components)/UsersPage";
+import Image from "next/image";
+import bgMap from "../public/bg-map.png"
 export default function Home() {
   return (
-    <main
-      data-theme="dark"
-      className="flex min-h-screen flex-col items-center justify-between p-24"
-    >
-      <header className="bg-[#1A1A1A]">
-        Introducing Guardian Hub – Your Personal Safety Network At Guardian Hub,
-        we believe in a world where people protect each other. Our mission is to
-        create a community of vigilant individuals connected by a shared
-        commitment to safety. Welcome to a platform where you can connect and
-        live more safely – Guardian Hub.
+    <main className="flex flex-col items-center justify-between ">
+      <header className="bg-[#1A1A1A] flex w-full min-h-screen justify-center relative">
+      <Image
+          src={bgMap}
+          alt="map background"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="absolute inset-0 z-0  backdrop-opacity-95 blur-[2px]"
+        />
+        <div className="max-w-7xl p-7 sm:p-9 relative z-10">
+          <h1>BlockGuardian</h1>
+          <p className="text-gray-100">
+            Introducing Guardian Hub – Your Personal Safety Network At Guardian
+            Hub, we believe in a world where people protect each other. Our
+            mission is to create a community of vigilant individuals connected
+            by a shared commitment to safety. Welcome to a platform where you
+            can connect and live more safely – Guardian Hub.
+          </p>
+        </div>
       </header>
-      <h1>Home</h1>
-      <UserReportList />
-      <UserList />
+      <main className=" h-screen pt-10 bg-white w-full flex justify-center ">
+
+      <h1 className="text-black ">Home</h1>
+      </main>
+      {/* <UserReportList />
+      <UserList /> */}
     </main>
   );
 }
