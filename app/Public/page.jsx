b4c14d8d1edf9db675f19b33a35668f16cee79e8
KrystianGH2 <kgcdronio@gmail.com>
2024-01-19 18:54:48 +0100

    testing page

--- app/Public/page.jsx
+++ app/Public/page.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import ReportForm from "../(components)/ReportForm";
+import UserReportList from "../(components)/UserReport";
 
 const Public = () => {
   return (
-    <div>
-      <h1>Public</h1>
+    <div className="flex flex-col  w-full items-center">
+    <div className=" max-w-7xl px-10 w-full">
+      <ReportForm />
     </div>
+
+      <UserReportList />
+  </div>
   );
 };
 
